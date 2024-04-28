6b48fdbffae45040217474af79e99378c31b2356
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2024-04-28 11:55:43 -0300

    separei o arquivo de estilo

--- src/pages/Tarefas/index.js
+++ src/pages/Tarefas/index.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Input } from 'react-native';
+import {styles} from './style';
+
 
 export default function Tarefas() {
   return (
     <View style={styles.container}>
-      <Text>Tarefas</Text>
+      <Text>Tarefas d</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.containerInput}>
+        <Text>Texto do conainer</Text>
+        {/* <Input placeholder="Informe tarefa aqui..."/> */}
+        </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ const Drawer = createDrawerNavigator();
 
 export default function Routes() {
   return (
-    <Drawer.Navigator initialRouteName="MeuDia">
+    <Drawer.Navigator initialRouteName="Tarefas">
       <Drawer.Screen name="MeuDia" component={MeuDia} />
       <Drawer.Screen name="Importante" component={Importante} />
       <Drawer.Screen name="Planejado" component={Planejado} />
