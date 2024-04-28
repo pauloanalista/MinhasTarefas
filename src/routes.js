515bae74ca482d1f3a53e98109bce8a102fbc95b
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2024-04-28 10:59:48 -0300

    adicionei novas paginas

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,20 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import MeuDia from './pages/MeuDia';
 import Importante from './pages/Importante';
+import Planejado from './pages/Planejado';
+import Tarefas from './pages/Tarefas';
+
 const Drawer = createDrawerNavigator();
 
 export default function Routes() {
   return (
-    
     <Drawer.Navigator initialRouteName="MeuDia">
       <Drawer.Screen name="MeuDia" component={MeuDia} />
       <Drawer.Screen name="Importante" component={Importante} />
+      <Drawer.Screen name="Planejado" component={Planejado} />
+      <Drawer.Screen name="Tarefas" component={Tarefas} />
     </Drawer.Navigator>
+    
   );
 }
 
